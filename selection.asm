56263d7405f2bd8722353660f0a3cabeb8a834a9
Austin Staton <47366235+aj-staton@users.noreply.github.com>
2019-03-05 00:42:16 -0500

    Add files via upload

---
+++ selection.asm
@@ -0,0 +1,39 @@
+# Written by Austin Staton
+# Used in Intro. to Computer Architecture (CSCE 212) at the UofSC
+# Translation of (from C):
+#  if (a < 20) {
+#	val = (val & 0xFFFF) * 16;
+#  } else {
+#       val = (val >> 16) + 10;
+#  }
+
+.data
+	a: 	.word 19
+	val:	.word 10
+.text
+	lw  $t0, a
+	bgt $t0, 20, else	# If a < 20, go to the else statement. 
+	lw  $s0, val		# Get val from memory.
+	and $s0, $s0, 0xFFFF	# AND val and 0xFFFF (111...1111) = val
+	mul $s0, $s0, 16	# $s0 = val*16
+	sw  $s0, val
+return:
+	#***Print to Check - Ignore for solution***
+	li   $v0, 1
+	lw   $s2, val
+	add  $a0, $zero, $s2
+	syscall
+	#******************************************
+	
+	li   $v0, 10 		# End program call.
+	syscall
+	
+else:	lw   $s0, val	      	# Unsure of double $s0 in here and symbol
+	srl  $s0, $s0, 16	# shift right 16 bits, 2^4
+	addi $s0, $s0, 10
+	sw   $s0, val
+	j return
+	
+	
+	
+	
